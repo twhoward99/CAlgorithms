85569b627a25419bf8b1b6c80d2ad9ca25301304
Tom Howard <thomaswhoward@gmail.com>
2018-03-30 20:59:34 -0400

    Initial Commit

---
+++ CAlgorithms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CAlgorithms
+//
+//  Created by Tom Howard on 3/30/18.
+//  Copyright © 2018 Tom Howard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
